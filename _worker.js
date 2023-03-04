e68db0db17b245c227f7cbaa7d33b6ede3a07ac0
Bill <liboning2011@163.com>
2023-03-04 15:10:11 +0800

    Create _worker.js
    CDN file commit（v1.0.0)

---
+++ _worker.js
@@ -0,0 +1,12 @@
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="cdn.jsdelivr.net";  //你需要反代的域名
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // 否则，提供静态资产。
+      return env.ASSETS.fetch(request);
+    }
+  };
