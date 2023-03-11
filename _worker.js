95365b21c6987636c09d5033cc9c79d4db2f87c9
WindBack <97789192+wndbac@users.noreply.github.com>
2023-03-11 13:06:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="cdn.jsdelivr.net";  //你需要反代的域名
+        url.hostname="unpkg.com";  // 你需要反代的域名
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
