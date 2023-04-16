2496cd9ff893bd56feea8cfd2e0c2746ffe25fc6
WindBack <97789192+wndbac@users.noreply.github.com>
2023-04-16 12:12:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,12 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    url.host = "cdn.jsdelivr.net";
-    // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
-  }
-}
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="cdn.jsdelivr.net"; // 需要反代的域名
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // 否则，提供静态资产。
+      return env.ASSETS.fetch(request);
+    }
+  };
